3e91e6522879d8ef0f8a55d13be7932d25c4a4bc
CaptainNemo <rohitashwachaks@gmail.com>
2025-12-01 01:23:09 +0530

    bash scripts

---
+++ run_b.sh
@@ -0,0 +1,9 @@
+# Backend
+# Mongo
+# docker run --name mongodb -p 27017:27017 -v /Users/rchaks/Code/GitHub/megamind/backend/data:/data/db mongo
+
+# Python
+# python3 -m venv .venv && source .venv/bin/activate
+# pip install -r backend/requirements.txt
+cd backend
+python app.py  # serves http://localhost:8000/api/v1
